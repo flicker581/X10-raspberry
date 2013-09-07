3ddb3282de0a437a3f44b6cfe10d0b0321032901
Pavel Levshin <pavel@levshin.spb.ru>
2013-09-07 12:46:48 +0400

    Some current version with bidirectional capability

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 //#include <avr/eeprom.h>
 #include <string.h>
 #include <avr/pgmspace.h>
+#include <util/crc16.h>
 
 #define DDR_SPI DDRB
 #define PORT_SPI PORTB
@@ -24,29 +25,51 @@
 
 #define T1_TICKS(us) ((uint8_t)(1.0*F_CPU/64*us/1000000))
 #define X10_SAMPLE_DELAY 500
+#define X10_TRANSMIT_LENGTH 1000
 
-#define X10_STATE_IDLE 0
-#define X10_STATE_COMMAND 1
-#define X10_STATE_EXTENDED 2
-#define X10_STATE_RECEIVED 3
+#define REQUEST_POLL 0
+#define REQUEST_CANCEL 1
+#define REQUEST_TRANSMIT 2
 
+#define RESPONSE_INPROGRESS 1
+#define RESPONSE_COMPLETE 2
 
 typedef struct _struct_spi_status {
  uint8_t running : 1;
- uint8_t rx_ready : 1;
- uint8_t tx_ready : 1;
-} spi_status_type;
+ uint8_t rx_body : 1;
+ uint8_t rx_done : 1;
+ uint8_t tx_enabled : 1;
+} spi_status_t;
 
-volatile spi_status_type spi_status;
+// Maximum is 32 due to stream_tail size, but RAM restricts it further
+// 24 is reasonable minimum due to extended command size being 22*2+6+62*2=174 bits
+#define X10_BITSTREAM_OCTETS 24
 
-volatile uint8_t spi_rx_buffer[38];
-volatile uint8_t spi_tx_buffer[38];
-volatile uint8_t spi_index;
+typedef struct _x10_bitstream {
+	uint8_t data[X10_BITSTREAM_OCTETS];
+	uint8_t tail; // pointer to the bit after the stream
+ } x10_bitstream_t;
 
-volatile uint32_t x10_rx;
-volatile uint8_t x10_tmp;
-volatile uint8_t x10_state = X10_STATE_IDLE;
-volatile uint8_t x10_bits = 0;
+typedef struct _spi_message {
+ uint8_t rr_code;
+ uint8_t rr_id;
+ x10_bitstream_t x10_data;
+ uint16_t crc16;
+} spi_message_t;
+
+volatile spi_status_t spi_status;
+
+volatile spi_message_t spi_rx_message;
+spi_message_t spi_tx_message;
+
+x10_bitstream_t tx_bitstream; // currently transmitted
+
+uint8_t spi_counter; // counts bytes in a SPI transaction, up to spi_message size
+
+volatile uint8_t x10_rx;
+volatile uint8_t x10_tx;
+volatile uint8_t x10_rx_counter = 0;
+volatile uint8_t x10_tx_counter = 0;
 
 static void spi_enable(void) {
  // MISO is output
@@ -57,10 +80,10 @@ static void spi_enable(void) {
  // Clear clock counter (set to 0)
  // Clear counter overflow flag
  USISR = _BV(USIOIF);
- // Place first octet to send
- USIDR = (spi_status.tx_ready) ? spi_tx_buffer[0] : 0xfe;
- // Init buffer
- spi_index = 0;
+ // Preload first octet to send
+ USIDR = (spi_status.tx_enabled) ? ((uint8_t*)&spi_tx_message)[0] : 0xFE;
+ // Init transaction engine
+ spi_counter = 0;
  // Raise flag
  spi_status.running = 1;
  // start the timer
@@ -74,7 +97,9 @@ static void spi_disable(void) {
  // and SPI is disabled
  USICR = 0;
  spi_status.running = 0;
- spi_status.rx_ready = 1;
+ if (spi_status.rx_body) {
+  spi_status.rx_done = 1;
+ }
 }
 
 /*
@@ -99,11 +124,29 @@ ISR(IO_PINS_vect) {
 
 ISR(USI_OVF_vect) {
  
- spi_rx_buffer[spi_index] = USIDR;
- if (++spi_index >= sizeof(spi_rx_buffer)) {
-  spi_index = 0;
+ // Time-critical section
+ uint8_t tmp_rx = USIDR;
+
+  // tx for index 1..sizeof(spi_message_t)-1
+  // and only if tx_enabled is set
+ USIDR = (spi_status.tx_enabled && (spi_counter < sizeof(spi_message_t)-1)) ? 
+  ((uint8_t*)&spi_tx_message)[spi_counter+1] : 0xFE;
+ 
+ // End of time-critical section
+
+ // Do not overwrite previous request if the new request is POLL
+ if (spi_counter == 0) {
+  spi_status.rx_body = (tmp_rx == REQUEST_POLL) ? 0 : 1;
  }
- USIDR = (spi_status.tx_ready) ? spi_tx_buffer[spi_index] : 0xFE;
+    
+ if (spi_counter<sizeof(spi_message_t)) {
+  // rx for index 0..sizeof(spi_message_t)-1
+  if (spi_status.rx_body) {
+   ((uint8_t*) &spi_rx_message)[spi_counter] = tmp_rx;
+  }
+  ++spi_counter;
+ }
+ 
  USISR |= _BV(USIOIF);
 }
 
@@ -118,20 +161,44 @@ ISR(INT0_vect) {
  OCR1A = TCNT1 + T1_TICKS(X10_SAMPLE_DELAY);
  TIMSK |= _BV(OCIE1A);
  TIFR = _BV(OCF1A);
+ // Transmit
+ if (x10_tx_counter) {
+  if (x10_tx & 0x80) {
+   PORT_X10 |= _BV(X10_OUT);
+   OCR1B = TCNT1 + T1_TICKS(X10_TRANSMIT_LENGTH);
+   TIMSK |= _BV(OCIE1B);
+   TIFR = _BV(OCF1B);
+  } 
+  x10_tx <<= 1;
+  --x10_tx_counter;
+ }
 }
 
 /*
  *
- * Time to sample a x10 bit
+ * Time to switch off X10 output
+ *
+ */
+
+ISR(TIMER1_CMPB_vect) {
+
+ // End of X10 pulse
+ PORT_X10 &= ~_BV(X10_OUT);
+ 
+ // One-shot interrupt
+ TIMSK &= ~_BV(OCIE1B);
+}
+
+/*
+ *
+ * Time to sample a X10 bit
  *
  */
 
 ISR(TIMER1_CMPA_vect) {
  
  x10_rx = (x10_rx << 1) + ( bit_is_clear(PIN_X10, X10_IN) ? 1 : 0 );
- if (!((++x10_bits) & 0x7)) {
-  x10_state = X10_STATE_RECEIVED;
- }
+ x10_rx_counter++;
  /*
  x10_tmp <<= 1;
  if (bit_is_clear(PIN_X10, X10_IN)) {
@@ -196,35 +263,47 @@ static void spi_init(void) {
  GIMSK |= _BV(PCIE0);
 }
 
-inline uint8_t spi_rx_ready(void) {
- return spi_status.rx_ready;
-}
-
-inline void spi_rx_wait(void) {
- while (!spi_status.rx_ready);
-}
-
-inline void spi_cleanup(void) {
+inline void spi_clear_rx_done(void) {
  uint8_t tmp_sreg = SREG;
  
  cli();
- spi_status.rx_ready = 0;
+ spi_status.rx_done = 0;
  SREG = tmp_sreg;
 }
 
-inline void spi_enable_tx(void) {
- uint8_t tmp_sreg = SREG;
+/*
+ * Checksum the message
+ */
+
+uint16_t spi_crc16(spi_message_t *spi_buffer) {
+ uint16_t crc = 0xffff;
  
+ for (uint8_t i=0; i<sizeof(spi_message_t)-2; i++) {
+  crc = _crc16_update(crc, ((uint8_t*)spi_buffer)[i]);
+ }
+ return crc;
+}
+
+/*
+ * Enable SPI transmission
+ */
+
+static void spi_enable_tx(void) {
+ uint8_t tmp_sreg = SREG;
+
+ spi_tx_message.crc16 = spi_crc16( (spi_message_t*)&spi_tx_message );
+
  cli();
- spi_status.tx_ready = 1;
+ spi_status.tx_enabled = 1;
  SREG = tmp_sreg;
 }
 
-inline void spi_disable_tx(void) {
+static void spi_disable_tx(void) {
  uint8_t tmp_sreg = SREG;
  
+ while (spi_status.running); // Wait while SPI is active
  cli();
- spi_status.tx_ready = 1;
+ spi_status.tx_enabled = 0;
  SREG = tmp_sreg;
 }
 
@@ -237,7 +316,12 @@ static void x10_init(void) {
 }
 
 int main(void) {
- uint8_t index = 0;
+ uint8_t rx_x10_index = 0;
+ struct _x10_tx_state {
+  uint8_t has_bitstream : 1;
+  uint8_t has_additional_rq : 1;
+  uint8_t bitstream_index : 5;
+ } x10_tx_state;
 
  //pullup all unused pins at PORTA
  DDRA = 0x00;
@@ -246,22 +330,82 @@ int main(void) {
  spi_init();
  x10_init();
  
+ x10_tx_state.has_bitstream = 0;
+ 
  sei();
+ spi_enable_tx();
  while (1) {
-  //spi_rx_wait();
-  if (x10_state == X10_STATE_RECEIVED) {
-   spi_tx_buffer[index] = x10_rx;
-   if (index < 36) {
-    index++;
+  
+  if (x10_rx_counter >= 8) {
+   spi_disable_tx(); // we will modify spi_tx_message.
+   cli(); // delay x10 interrupts, just in case...
+   spi_tx_message.x10_data.data[rx_x10_index] = x10_rx;
+   x10_rx_counter = 0;
+   sei();
+   if (++rx_x10_index >= X10_BITSTREAM_OCTETS) {
+    rx_x10_index = 0;
+   }
+   spi_tx_message.x10_data.tail = rx_x10_index * 8;
+  }
+
+  // if we have data to transmit and X10 transmission is over
+  if (x10_tx_state.has_bitstream && !x10_tx_counter) {
+   if (tx_bitstream.tail == 0) {
+    // This transmission is over
+    spi_disable_tx();
+    spi_tx_message.rr_code = RESPONSE_COMPLETE;
+	x10_tx_state.has_bitstream = 0;
    }
    else {
-    index = 0;
+    // The transmission is not finished yet.
+    cli(); // delay X10 interrupts
+	x10_tx = tx_bitstream.data[x10_tx_state.bitstream_index++];
+	if (tx_bitstream.tail < 8) {
+	 // this is incomplete octet
+	 x10_tx_counter = tx_bitstream.tail;
+	 tx_bitstream.tail = 0;
+	}
+	else {
+	 // one more complete octet to send
+	 x10_tx_counter = 8;
+	 tx_bitstream.tail -= 8;
+	}
+	sei();
    }
-   spi_tx_buffer[36] = index;
-   x10_rx = 0;
-   x10_state = X10_STATE_IDLE;
   }
+
+  if (spi_status.rx_done ||
+   // this is not correct. SPI can be in transmission right here, and the buffer can change
+   ( x10_tx_state.has_additional_rq && !x10_tx_state.has_bitstream )) {
+   if (spi_crc16((spi_message_t*)&spi_rx_message) == spi_rx_message.crc16) {
+    // CRC is correct
+	spi_disable_tx();
+	switch (spi_rx_message.rr_code) {
+     case REQUEST_CANCEL: 
+	  x10_tx_state.has_bitstream = 0;
+	  spi_tx_message.rr_id = spi_rx_message.rr_id;
+	  spi_tx_message.rr_code = RESPONSE_COMPLETE;
+	  break;
+	 case REQUEST_TRANSMIT:
+	  if (!x10_tx_state.has_bitstream) {
+       tx_bitstream = spi_rx_message.x10_data;
+	   x10_tx_state.has_bitstream = 1;
+	   x10_tx_state.bitstream_index = 0;
+	   x10_tx_state.has_additional_rq = 0;
+       spi_tx_message.rr_id = spi_rx_message.rr_id;
+	   spi_tx_message.rr_code = RESPONSE_INPROGRESS;
+	  }
+	  else {
+	   // the request should be chained, where possible
+	   x10_tx_state.has_additional_rq = 1; 
+	  }
+	  break;
+	  // Default is to ignore the request
+	}
+   }
+   spi_clear_rx_done();
+  }
+
   spi_enable_tx();
-  spi_cleanup();
  }
 }
